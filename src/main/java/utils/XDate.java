c3a63f4762ce29c5dcd20ca2a2620513b2b41105
Phan Lam Hung <lamhungphan1999@gmail.com>
2024-07-07 16:35:26 +0700

    update test

---
+++ src/main/java/utils/XDate.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Computer
+ */
+public class XDate {
+
+    static SimpleDateFormat formater = new SimpleDateFormat();
+
+    /**
+     * Chuyển đổi String sang Date
+     *
+     * @param date là String cần chuyển
+     * @param pattern là định dạng thời gian
+     * @return Date kết quả
+     */
+    public static Date toDate(String date, String patten) throws ParseException {
+
+        formater.applyPattern(patten);
+        return formater.parse(date);
+    }
+
+    /**
+     * Chuyển đổi từ Date sang String
+     *
+     * @param date là Date cần chuyển đổi
+     * @param pattern là định dạng thời gian
+     * @return String kết quả
+     */
+    public static String toString(Date date, String patten) {
+        formater.applyPattern(patten);
+        return formater.format(date);
+    }
+
+    /**
+     * Bổ sung số ngày vào thời gian
+     *
+     * @param date thời gian hiện có
+     * @param days số ngày cần bổ sung váo date
+     * @return Date kết quả
+     */
+    public static Date addDays(Date date, long days) {
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        return date;
+    }
+}
---
+++ src/main/java/utils/XImage.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Computer
+ */
+public class XImage {
+
+    /**
+     * Ảnh biểu tượng của ứng dụng, xuất hiện trên mọi cửa sổ
+     *
+     * @return
+     */
+//    public static Image getAppIcon() {
+//        String file = "/icon/fpt.png";
+//        URL location = XImage.class.getResource(file);
+//        Image icon = new ImageIcon(location).getImage();
+//        return icon;
+//    }
+
+    /**
+     * Sao chép file logo chuyên đề vào thư mục logo
+     *
+     * @param src là đối tượng file ảnh
+     */
+    public static void save(File src) {
+        File dst = new File("Images", src.getName());
+        if (!dst.getParentFile().exists()) {
+            dst.getParentFile().mkdirs();
+        }
+        try {
+            Path from = Paths.get(src.getAbsolutePath());
+            Path to = Paths.get(dst.getAbsolutePath());
+
+            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+     /**
+     * Đọc hình ảnh logo chuyên đề
+     * @param fileName  là tên file logo
+     * @return ảnh đọc được
+     */   
+    public static ImageIcon read(String fileName){
+        File path = new File("Images", fileName);
+        return new ImageIcon(path.getAbsolutePath());
+    }
+}
