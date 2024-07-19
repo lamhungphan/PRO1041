2ee78d1e3d382990513712f6a750c8a2a525813c
devlam <havulam1998z@gmail.com>
2024-07-19 19:17:10 +0700

    feat/mail

---
+++ src/main/java/com/mail/Gmailler.java
@@ -0,0 +1,46 @@
+package com.mail;
+import java.util.Properties;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+public class Gmailler {
+
+    public static void sendEmail(String to, String from, String username, String password, String host, String subject, String content) {
+        //cấu hình mailtrap
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+
+        //tạo phiên object
+        Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            //create a MimeMessage object
+            Message message = new MimeMessage(session);
+            //set From email field
+            message.setFrom(new InternetAddress(from));
+            //set To email field
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            //set email subject field
+            message.setSubject(subject);
+            //set content của mail
+            message.setText(content);
+            //send the email message
+            Transport.send(message);
+            System.out.println("Email Message Sent Successfully");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
